d8c18b853ff302b56d1522510a769043214b0f53
Loukky <ly123loukky@gmail.com>
2020-08-11 08:43:35 +0800

    Update cfcdn.js

--- themes/cfcdn.js
+++ themes/cfcdn.js
@@ -12,3 +12,26 @@ $.ajax({
         $('#ftinfo').append(`加载耗时：<code>${endTime-startTime}ms</code>`); 
     }
 });  
+$.ajax({
+    url: "https://myip.ipip.net/json",
+    async: true,
+    type: "GET",
+    success: function(data, status, xhr) {
+        $('#userip-c').html(data.data.ip);
+        $('#userlocation-c').html(`${data.data.location[0]} ${data.data.location[1]} ${data.data.location[2]}`);
+        if(data.data.location[4]) $('#userisp-c').html(data.data.location[4]);
+        else $('#userisp').css("display", "none");
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    },
+    error: function(xhr, status, error) {
+        $("#userip").css("display", "none");
+        $('#userlocation').css("display", "none");
+        $('#userisp').css("display", "none");
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    }
+});
+
+setInterval(function(){ $('#usernowtime').html(`${new Date()}`) }, 100);
+$('#usersys').html(getUserOSInfo());
