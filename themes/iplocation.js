d5bc6a4b1df40a8bc896f7b5ca53384dd8d6fe7b
Loukky <ly123loukky@gmail.com>
2021-08-18 23:40:18 +0800

    Update iplocation.js

--- themes/iplocation.js
+++ themes/iplocation.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
     function vfhkyipquery(){
         $(".vfhkyipquery").each(function(){
-            var c=$.trim($(this).attr("data-ip")),t=$(this),i="";
+            var c=$.trim($(this).attr("data-ip")),i="";
             $.getScript("https://ip.zxinc.org/api.php?type=js&ip="+c,function(){
                 if(returnIpData.ret==1){
                     var h=returnIpData.data.ip.location;
