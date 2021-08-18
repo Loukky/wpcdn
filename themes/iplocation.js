9c281c2922299fbf015af967fa1d3d6081dbe367
Loukky <ly123loukky@gmail.com>
2021-08-18 22:34:17 +0800

    Update iplocation.js

--- themes/iplocation.js
+++ themes/iplocation.js
@@ -2,12 +2,11 @@ jQuery(document).ready(function($){
     function vfhkyipquery(){
         $(".vfhkyipquery").each(function(){
             var c=$.trim($(this).attr("data-ip")),t=$(this),i="";
-            $.getScript("http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js&ip="+c,function(){
+            $.getScript("https://ip.zxinc.org/api.php?type=js&ip="+c,function(){
                 if(remote_ip_info.ret==1){
-                    var h=remote_ip_info.country,f=remote_ip_info.province,e=remote_ip_info.city,g=" "+remote_ip_info.isp;
+                    var h=remote_ip_info.data.ip.location;
                     i+=h;
-                    i+=f==e?e:f+e;
-                    i+=g;
+                   
                 }
                 else{
                     i="未知IP地址";
