e1cf114f13e7e27ebb3ce2b33fa6c4f886b78fd4
Loukky <ly123loukky@gmail.com>
2021-08-31 17:47:40 +0800

    Update http2https.js

--- themes/http2https.js
+++ themes/http2https.js
@@ -1,4 +1,3 @@
-	var targetProtocol = "https:";
-	if (window.location.protocol != targetProtocol)
-		window.location.href = targetProtocol +
-		window.location.href.substring(window.location.protocol.length);
+if (location.protocol !== "https:"){
+location.replace(window.location.href.replace("http:", 
+"https:"));}
