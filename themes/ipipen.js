43c5c99e2577aeba909545c211a6b7a5191fd548
Loukky <ly123loukky@gmail.com>
2021-09-12 18:36:56 +0800

    Create ipipen.js

---
+++ themes/ipipen.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+
+$.ajax({
+    url: "https://api.myip.la/en?json",
+    async: true,
+    type: "GET",
+    success: function(data, status, xhr) {
+        $('#userip-c').html(data.ip);
+        $('#userlocation-c').html(`${data.location.city} ${data.location.province} ${data.data.location.country_name}`);
+        /*if(data.data.location[4]) $('#userisp-c').html(data.data.location[4]);
+        else $('#userisp').css("display", "none");*/
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    },
+    error: function(xhr, status, error) {
+        $("#userip").css("display", "none");
+        $('#userlocation').css("display", "none");
+        $('#userisp').css("display", "none");
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    }
+});
+});
