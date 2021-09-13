3a192193bbc14d48e8c1662adb63f0b66b8c24bc
Loukky <ly123loukky@gmail.com>
2021-09-14 00:07:04 +0800

    Update embed.js

--- imgur-pro/static/embed.js
+++ imgur-pro/static/embed.js
@@ -24,7 +24,8 @@ layui.use(['upload','form','element','layer','flow'], function(){
         //图片查看器
         layer.photos({
             photos: '#found'
-            ,anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+            ,anim: 5, //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+		closeBtn:1
         });
         layer.photos({
             photos: '#lightgallery'
