9b1b2a9fd570f7771053aa59b6466727240aea82
Loukky <ly123loukky@gmail.com>
2021-09-21 02:25:10 +0800

    Create ip-api.js

---
+++ themes/ip-api.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($) {
+
+$.ajax({
+    url: "http://ip-api.com/json/",
+    async: true,
+    type: "GET",
+    success: function(data, status, xhr) {
+        $('#userip-c').html(query);
+        $('#userlocation-c').html(`${regionName} , ${country} , ${isp}`);
+        
+        if(data.location.city) $('#userlocation-c').html(`${city} , ${regionName} , ${country} , ${isp}`);
+        else $('#userlocation-c').html(`${regionName} , ${country} , ${isp}``);
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    },
+    error: function(xhr, status, error) {
+        $("#userip").css("display", "none");
+        $('#userlocation').css("display", "none");
+        $('#userisp').css("display", "none");
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    }
+});
+});
