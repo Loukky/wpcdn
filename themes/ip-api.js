3d5805d9166f60080449fe006d9d01f33c86263c
Loukky <ly123loukky@gmail.com>
2021-09-21 02:59:12 +0800

    Update ip-api.js

--- themes/ip-api.js
+++ themes/ip-api.js
@@ -8,7 +8,7 @@ $.ajax({
         $('#userip-c').html(data.ip);
         $('#userlocation-c').html(`${data.region} , ${data.country} , ${data.isp}`);
         
-        if(data.location.city) $('#userlocation-c').html(`${data.city} , ${data.region} , ${data.country} , ${data.isp}`);
+        if(data.city) $('#userlocation-c').html(`${data.city} , ${data.region} , ${data.country} , ${data.isp}`);
         else $('#userlocation-c').html(`${data.region} , ${data.country} , ${data.isp}`);
         $('#loading-tab2').css("display", "none");
         $('#tab2-s').css("display", "inline-block");
