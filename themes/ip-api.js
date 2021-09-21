84ef9bfd74718f4993fa6d675c7269862494317d
Loukky <ly123loukky@gmail.com>
2021-09-22 00:23:30 +0800

    Update ip-api.js

--- themes/ip-api.js
+++ themes/ip-api.js
@@ -3,16 +3,16 @@ jQuery(document).ready(function($) {
 function(json1){
       var ip = (json1.ip);
 $.ajax({
-    //url: 'https://ipwhois.app/json/'+ ip,
-    url: 'https://ipapi.co/' + ip + '/json/',
+    url: 'https://ipwhois.app/json/'+ ip,
+    //url: 'https://ipapi.co/' + ip + '/json/',
     async: true,
     type: "GET",
     success: function(data, status, xhr) {
         $('#userip-c').html(data.ip);
-        $('#userlocation-c').html(`${data.region} , ${data.country} , ${data.org}`);
+        $('#userlocation-c').html(`${data.region} , ${data.country} , ${data.isp}`);
         
-        if(data.city) $('#userlocation-c').html(`${data.city} , ${data.region} , ${data.country} , ${data.org}`);
-        else $('#userlocation-c').html(`${data.region} , ${data.country} , ${data.org}`);
+        if(data.city) $('#userlocation-c').html(`${data.city} , ${data.region} , ${data.country} , ${data.isp}`);
+        else $('#userlocation-c').html(`${data.region} , ${data.country} , ${data.isp}`);
         $('#loading-tab2').css("display", "none");
         $('#tab2-s').css("display", "inline-block");
     },
