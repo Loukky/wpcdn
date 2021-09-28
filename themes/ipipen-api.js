7faccbe521f0420ba4e83c17c5d54ecc4baaab9e
Loukky <ly123loukky@gmail.com>
2021-09-28 21:21:21 +0800

    Create ipipen-api.js

---
+++ themes/ipipen-api.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+  $.get("https://api.myip.la/?callback=?",  
+function(text){
+      var ip = (text);
+$.ajax({
+    url: 'https://ipwhois.app/json/'+ ip +'?objects=country,city,isp,ip,region',
+    //url: 'https://ipapi.co/' + ip + '/json/',
+    async: true,
+    type: "GET",
+    success: function(data, status, xhr) {
+        $('#userip-c').html(data.ip);
+        $('#userlocation-c').html(`${data.region} , ${data.country} , ${data.isp}`);
+        
+        if(data.city) $('#userlocation-c').html(`${data.city} , ${data.region} , ${data.country} , ${data.isp}`);
+        else $('#userlocation-c').html(`${data.region} , ${data.country} , ${data.isp}`);
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    },
+    error: function(xhr, status, error) {
+        $("#userip").css("display", "none");
+        $('#userlocation').css("display", "none");
+        $('#userisp').css("display", "none");
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    }
+});
+      }
+	   );
+});
