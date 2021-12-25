8b59c1a97332d6e4f0fb5b58c2146a67d912302a
Loukky <ly123loukky@gmail.com>
2021-12-26 00:59:50 +0800

    Update ip2contryflags.js

--- themes/ip2contryflags.js
+++ themes/ip2contryflags.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
     function vfhkyconlocquery(){
         $(".vfhkyconlocquery").each(function(){
-            var c=$.trim($(this).attr("loc-ip")),t=$(this),i="",m="";
+            var c=$.trim(atob($(this).attr("loc-ip"))),t=$(this),i="",m="";
             $.getScript("https://geoip.loukky.com/?js=1&ip="+c,function(){
                 if(returnIpData.code==0){
                     var h=returnIpData.data.code2;
