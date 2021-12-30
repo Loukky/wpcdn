dede22c341706f67d46b1b31205b44a06603ae6a
Loukky <ly123loukky@gmail.com>
2021-12-30 22:20:46 +0800

    Update ipzx-api.js

--- themes/ipzx-api.js
+++ themes/ipzx-api.js
@@ -14,7 +14,7 @@ jQuery(document).ready(function($){
                     i="未知归属地";
 					m="未知IP";
                 }
-                t.replaceWith('<span>回源IP：<code>'+n+'</code> 归属于<code>'+i+'</code></span>");
+                t.replaceWith('<span>回源IP：<code>'+n+'</code> 归属于<code>'+i+'</code></span>');
             })
         })
     }
