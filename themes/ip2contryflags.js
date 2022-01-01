bf26ee953233f8758538c16fee9f34cf0b697e6f
Loukky <ly123loukky@gmail.com>
2022-01-01 23:31:02 +0800

    Update ip2contryflags.js

--- themes/ip2contryflags.js
+++ themes/ip2contryflags.js
@@ -16,7 +16,7 @@ jQuery(document).ready(function($){
                     m="unknow";
                 }
                 // t.replaceWith('<span class="ipinfo" style="font-family:roboto,sans-serif,serif">'+i+"</span>");
-                t.replaceWith('<span class="country-flag"><img src="data:image/svg+xml,%3Csvg%20xmlns=&apos;http://www.w3.org/2000/svg&apos;%20viewBox=&apos;0%200%2048%2048&apos;%3E%3C/svg%3E" class="avatar avatar-48 photo" height="48" width="48" data-lazy-src="https://cdn.jsdelivr.net/gh/Loukky/wpcdn/themes/show-useragent/flags/'+i+'.png" title="'+m+'" alt="'+m+'"></span>' );
+                t.replaceWith('<span class="country-flag"><img class="rocket-lazyload" src="https://cdn.jsdelivr.net/gh/Loukky/wpcdn/themes/show-useragent/flags/'+i+'.png" title="'+m+'" alt="'+m+'"></span>' );
             })
         })
     }
