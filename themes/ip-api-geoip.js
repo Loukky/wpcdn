efe6aef947bf887e1531247090caf71fcfc1937d
Loukky <ly123loukky@gmail.com>
2022-03-15 17:10:18 +0800

    Create ip-api-geoip.js

---
+++ themes/ip-api-geoip.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($) {
+  $.get("https://geoip.loukky.com/myip.php",
+function(text){
+      var ip = (text);
+$.ajax({
+    url: 'https://geoip.loukky.com/?ip='+ip,
+    async: true,
+    type: "GET",
+    success: function(data, status, xhr) {
+        $('#userip-c').html(data.ip);
+        $('#userlocation-c').html(`${data.city} , ${data.province} , {data.country}`);
+        if(data.data.myip); //$('#userlocation-c').html(`${data.country} , ${data.region} , ${data.city} , ${data.isp}`);
+        else $("#userip").css("display", "none");
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    },
+    error: function(xhr, status, error) {
+        $("#userip").css("display", "none");
+        $('#userlocation').css("display", "none");
+        $('#userisp').css("display", "none");
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    }
+});
+      }
+	   );
+});
