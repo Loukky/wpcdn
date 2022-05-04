f4405bf7b77e4f63a282b0583c02ccf4d0d7361c
Loukky <ly123loukky@gmail.com>
2022-05-04 21:18:06 +0800

    Create ip-api-ip2location.js

---
+++ themes/ip-api-ip2location.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($) {
+  $.get("https://geoip.loukky.com/myip.php",
+function(text){
+      var ip = (text);
+$.ajax({
+    url: 'https://geoip.loukky.com/ip2location/?ip='+ip,
+    async: true,
+    type: "GET",
+    success: function(data, status, xhr) {
+        $('#userip-c').html(data.ip);
+        $('#userlocation-c').html(`${data.city} , ${data.country}`);
+        if(data.province) $('#userlocation-c').html(`${data.city} , ${data.province} , ${data.country}`);
+        else 
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    },
+    error: function(xhr, status, error) {
+        $("#userip").css("display", "none");
+        $('#userlocation').css("display", "none");
+        $('#userisp').css("display", "none");
+        $('#loading-tab2').css("display", "none");
+        $('#tab2-s').css("display", "inline-block");
+    }
+});
+      }
+	   );
+});
