436e31c719a4cc8e7bad088066bfc2ffb3ddc722
Loukky <ly123loukky@gmail.com>
2022-10-27 01:36:34 +0800

    Update cloudflarecdnnode.js

--- themes/cloudflarecdnnode.js
+++ themes/cloudflarecdnnode.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 $.get("/cdn-cgi/trace",function(data){
 //sip = data.match(/(ip=+)(\S*)/)[2];  
 str = data.match(/(colo=?)(\S*)/)[2];  
-//$("#result").append("您的IP:"+sip);
+//$("#cdnresult").append("您的IP:"+sip);
 $("#cdnresult").append(str);
 });
 });
