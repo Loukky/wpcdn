b61aa5eceaabcd0101c93e49d45b52ec8c57e5f0
Loukky <ly123loukky@gmail.com>
2022-10-27 01:32:43 +0800

    Create cloudflarecdnnode.js

---
+++ themes/cloudflarecdnnode.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$.get("/cdn-cgi/trace",function(data){
+//sip = data.match(/(ip=+)(\S*)/)[2];  
+str = data.match(/(colo=?)(\S*)/)[2];  
+//$("#result").append("您的IP:"+sip);
+$("#result").append("CDN Node:"+str);
+});
+});
