f108ee4176353557472108ed290e83b7351d67fc
Loukky <ly123loukky@gmail.com>
2022-11-27 02:24:11 +0800

    Update ip-api-ip2location.js

--- themes/ip-api-ip2location.js
+++ themes/ip-api-ip2location.js
@@ -7,19 +7,19 @@ $.ajax({
     async: true,
     type: "GET",
     success: function(data, status, xhr) {
-        $('#userip-c').html(data.ip);
-        $('#userlocation-c').html(`${data.city} , ${data.country}`);
-        if(data.province) $('#userlocation-c').html(`${data.city} , ${data.province} , ${data.country}`);
+        $("#userip-c").html(data.ip);
+        $("#userlocation-c").html(`${data.city} , ${data.country}`);
+        if(data.province) $("#userlocation-c").html(`${data.city} , ${data.province} , ${data.country}`);
         else 
-        $('#loading-tab2').css("display", "none");
-        $('#tab2-s').css("display", "inline-block");
+        $("#loading-tab2").css("display", "none");
+        $("#tab2-s").css("display", "inline-block");
     },
     error: function(xhr, status, error) {
         $("#userip").css("display", "none");
-        $('#userlocation').css("display", "none");
-        $('#userisp').css("display", "none");
-        $('#loading-tab2').css("display", "none");
-        $('#tab2-s').css("display", "inline-block");
+        $("#userlocation").css("display", "none");
+        $("#userisp").css("display", "none");
+        $("#loading-tab2").css("display", "none");
+        $("#tab2-s").css("display", "inline-block");
     }
 });
       }
